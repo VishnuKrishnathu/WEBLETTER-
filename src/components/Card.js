50fa5aecdfbfaa52b1013375cd7db91041729c2d
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2021-03-18 11:22:34 +0530

    new modifications

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <div className="title-content">
+        How to Invest Money
+        <span>-The Motley Fool</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import "../css/HomePage.css";
+import Card from "./Card";
+
+export default function HomePage() {
+  return (
+    <div className="main-content">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
